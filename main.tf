a326fd113c19e24c782d7d8770bc2a9b3ead659f
akandeae <104216596+akandeae@users.noreply.github.com>
2022-07-16 20:26:18 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -126,7 +126,7 @@ resource "aws_instance" "web-server-instance" {
     ami = "ami-0e472ba40eb589f49"
     instance_type = "t2.micro"
     availability_zone = "us-east-1a"
-    key_name = "main-key"
+    key_name = "my-new-key"
 
     network_interface {
         device_index = 0
